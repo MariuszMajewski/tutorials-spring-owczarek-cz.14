fb5321b9f85f1678c8f2da1c5b347a6e4182dba9
MrMario108 <mariusz.majewski@onet.pl>
2018-05-29 16:15:46 +0200

    Version by type. Tutorial's ended.

--- src/main/java/pl/mrmario/tutorials/spring/owczarek/cz14/Application.java
+++ src/main/java/pl/mrmario/tutorials/spring/owczarek/cz14/Application.java
@@ -4,25 +4,19 @@ public class Application {
 	private UserRepository userRepository;
 	private DictionaryRepository dictionaryRepository;
 	
-	public Application(UserRepository userRepository, DictionaryRepository dictionaryRepository) {
-		super();
-		this.userRepository = userRepository;
-		this.dictionaryRepository = dictionaryRepository;
-	}
-	
 	
 	public UserRepository getUserRepository() {
 		return userRepository;
 	}
-//	public void setUserRepository(UserRepository userRepository) {
-//		this.userRepository = userRepository;
-//	}
+	public void setUserRepository(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 	public DictionaryRepository getDictionaryRepository() {
 		return dictionaryRepository;
 	}
-//	public void setDictionaryRepository(DictionaryRepository dictionaryRepository) {
-//		this.dictionaryRepository = dictionaryRepository;
-//	}
+	public void setDictionaryRepository(DictionaryRepository dictionaryRepository) {
+		this.dictionaryRepository = dictionaryRepository;
+	}
 	
 	
 }
